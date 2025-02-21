1f40d43051991f145aadb3ab1251ed36e2b4bd7a
Poornimaa-devi <162299559+Poornimaa-devi@users.noreply.github.com>
2025-02-21 09:50:27 +0530

    Update removed task1.py

--- task1.py
+++ task1.py
@@ -2,7 +2,6 @@ def display_instructions():
     print("Welcome to the Brainy Quiz!")
     print("Here are the instructions:")
     print("---------------------------")
-    print("---------------------------")
     print("1. You will be asked a series of questions.")
     print("2. For each question, you will have multiple choices.")
     print("3. Type the letter corresponding to your chosen answer and press Enter.")
